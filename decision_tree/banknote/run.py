df444518065dd62b508f23fc6f309368bbca344a
qangstud <qangstud@gmail.com>
2020-09-23 23:10:35 +0700

    Initial commit

---
+++ decision_tree/banknote/run.py
@@ -0,0 +1,20 @@
+
+
+# Step 1: Gini index
+
+# Step 2: Spliting
+
+# Step 3: Build a Tree
+
+# Step 4: Make a Prediction
+
+# Step 5: Make multiple Prediction
+
+if __name__ == '__main__':
+    # Load banknote data set.
+
+    # Clean data
+
+    # Run steps
+
+    pass
