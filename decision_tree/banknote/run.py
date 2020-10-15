7dde80d74383894977de9ab38dd50702c4772d45
Quang Tran <tran.ducquang1308@gmail.com>
2020-10-15 23:24:06 +0700

    Added: Gini Index

--- decision_tree/banknote/run.py
+++ decision_tree/banknote/run.py
@@ -1,6 +1,18 @@
 
-
 # Step 1: Gini index
+def gini_index(groups, classes):
+    n_instances = float(sum([len(group) for group in groups]))
+    gini = 0.0
+    for group in groups:
+        size = float(len(group))
+        if size == 0:
+            continue
+        score = 0.0
+        for class_val in classes:
+            p = [row[-1] for row in group].count(class_val) / size
+        gini += (1.0 - score) * (size / n_instances)
+    return gini
+
 
 # Step 2: Spliting
 
@@ -11,6 +23,8 @@
 # Step 5: Make multiple Prediction
 
 if __name__ == '__main__':
+    print(gini_index([[[1, 1], [1, 0]], [[1, 1], [1, 0]]], [0, 1]))
+    print(gini_index([[[1, 0], [1, 0]], [[1, 1], [1, 1]]], [0, 1]))
     # Load banknote data set.
 
     # Clean data
