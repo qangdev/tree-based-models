1595f0a4c4541e9eaf2d0996c080dfc65f69c99c
tdquang <tdquang138k@gmail.com>
2020-10-27 16:26:30 +0700

    Added: Tennis use case

---
+++ decision_tree/tennis/run.py
@@ -0,0 +1,61 @@
+import csv
+from dataclasses import dataclass
+
+
+@dataclass
+class TennisRecord:
+    day: int
+    outlook: str
+    humidity: str
+    wind: str
+    play: str = None
+
+
+if __name__ == '__main__':
+    YES = "Yes"
+    NO = "No"
+
+    l0 = {"v": YES}
+    l1 = {"v": YES}
+    l2 = {"v": NO}
+    l3 = {"v": YES}
+    l4 = {"v": NO}
+
+    n2 = {"question": lambda val: val.wind == "Weak",
+          "l": l3,
+          "r": l4}
+
+    n1 = {"question": lambda val: val.humidity == "Normal",
+          "l": l1,
+          "r": l2}
+
+    n0 = {"question": lambda val: val.outlook == "Sunny",
+          "l": n1,
+          "r": n2}
+
+    tree = {"question": lambda val: val.outlook == "Overcast",
+            "l": l0,
+            "r": n0}
+
+    def predict(node, val):
+        if "v" in node:
+            return node["v"]
+        next_node = node["l"] if node["question"](val) else node["r"]
+        return predict(next_node,
+                       val)
+
+    with open("./data/tennis.csv", "r") as csv_file:
+        data = csv.reader(csv_file, delimiter=",")
+        data = [r for i, r in enumerate(data) if i != 0]
+        known_target = [TennisRecord(int(r[0]), r[1], r[2], r[3], r[4]) for i, r in enumerate(data)]
+        predict_target = []
+        for r in data:
+            record = TennisRecord(int(r[0]), r[1], r[2], r[3])
+            record.play = predict(tree, record)
+            predict_target.append(record)
+
+        for i in zip(known_target, predict_target):
+            if i[0].play != i[1].play:
+                print(i)
+
+        assert known_target == predict_target, "Ops, Something wrong"
